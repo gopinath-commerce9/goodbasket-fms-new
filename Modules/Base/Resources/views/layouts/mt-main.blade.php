3d595bcf3234f2c1033312c3b3546cce155632d3
Gopinath R <gopicommerce9@gmail.com>
2021-10-18 18:54:36 +0530

    Created a common base Layout with "Metronic Theme".

---
+++ Modules/Base/Resources/views/layouts/mt-main.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="description" content="Good Basket Fulfillment Center" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+        <title>@yield('page-title') - {{ config('app.name') }}</title>
+
+        <link rel="canonical" href="https://keenthemes.com/metronic" />
+        <!--begin::Fonts-->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+        <!--end::Fonts-->
+        <!--begin::Page Custom Styles(used by this page)-->
+        <link href="{{ asset('ktmt/css/pages/wizard/wizard-3.css') }}" rel="stylesheet" type="text/css" />
+
+        <link href="{{ asset('ktmt/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet" type="text/css" />
+
+        <!--end::Page Custom Styles-->
+        <!--begin::Global Theme Styles(used by all pages)-->
+        <link href="{{ asset('ktmt/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('ktmt/plugins/custom/prismjs/prismjs.bundle.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('ktmt/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+        <!--end::Global Theme Styles-->
+        <!--begin::Layout Themes(used by all pages)-->
+        <!--end::Layout Themes-->
+        <link rel="shortcut icon" href="{{ asset('ktmt/media/logos/favicon.png') }}" />
+
+        {{-- Laravel Mix - CSS File --}}
+        {{-- <link rel="stylesheet" href="{{ mix('css/base.css') }}"> --}}
+
+        @yield('custom-css-section')
+
+        @yield('initialize-js-section')
+
+    </head>
+
+    <body id="kt_body" class="header-fixed header-mobile-fixed subheader-enabled page-loading">
+
+        <!--begin::Main-->
+
+        <!--begin::Header Mobile-->
+
+        @include('base::layouts.partials.mt-mobile-header')
+
+        <!--end::Header Mobile-->
+
+        <div class="d-flex flex-column flex-root">
+
+            <!--begin::Page-->
+            <div class="d-flex flex-row flex-column-fluid page">
+
+                <!--begin::Aside-->
+                @include('base::layouts.partials.mt-sidebar')
+                <!--end::Aside-->
+
+                <!--begin::Wrapper-->
+                <div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
+
+                    <!--begin::Header-->
+                    @include('base::layouts.partials.mt-header')
+                    <!--end::Header-->
+
+                    <!--begin::Content-->
+                    <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+
+                        <!--begin::Subheader-->
+                        @include('base::layouts.partials.mt-subheader')
+                        <!--end::Subheader-->
+
+                        <!--begin::Entry-->
+                        <div class="d-flex flex-column-fluid">
+
+                            @yield('content')
+
+                        </div>
+                        <!--end::Entry-->
+
+                    </div>
+                    <!--end::Content-->
+
+                    <!--begin::Footer-->
+                    @include('base::layouts.partials.mt-footer')
+                    <!--end::Footer-->
+
+                </div>
+                <!--end::Wrapper-->
+
+            </div>
+            <!--end::Page-->
+
+        </div>
+        <!--end::Main-->
+
+        <script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>
+
+        <!--begin::Global Config(global config for global JS scripts)-->
+        <script>var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1200 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#8950FC", "secondary": "#E5EAEE", "success": "#1BC5BD", "info": "#8950FC", "warning": "#FFA800", "danger": "#F64E60", "light": "#F3F6F9", "dark": "#212121" }, "light": { "white": "#ffffff", "primary": "#E1E9FF", "secondary": "#ECF0F3", "success": "#C9F7F5", "info": "#EEE5FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#212121", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#ECF0F3", "gray-300": "#E5EAEE", "gray-400": "#D6D6E0", "gray-500": "#B5B5C3", "gray-600": "#80808F", "gray-700": "#464E5F", "gray-800": "#1B283F", "gray-900": "#212121" } }, "font-family": "Poppins" };</script>
+        <!--end::Global Config-->
+
+        <!--begin::Global Theme Bundle(used by all pages)-->
+        <script src="{{ asset('ktmt/plugins/global/plugins.bundle.js') }}"></script>
+        <script src="{{ asset('ktmt/plugins/custom/prismjs/prismjs.bundle.js') }}"></script>
+        <script src="{{ asset('ktmt/js/scripts.bundle.js') }}"></script>
+        <!--end::Global Theme Bundle-->
+
+        <!--begin::Page Vendors(used by this page)-->
+        <script src="{{ asset('ktmt/plugins/custom/datatables/datatables.bundle.js') }}"></script>
+        <!--end::Page Vendors-->
+
+        <!--begin::Page Scripts(used by this page)-->
+        <script src="{{ asset('ktmt/js/pages/crud/datatables/basic/basic.js') }}"></script>
+
+        {{-- Laravel Mix - JS File --}}
+        {{-- <script src="{{ mix('js/base.js') }}"></script> --}}
+
+        @yield('custom-js-section')
+        <!--end::Page Scripts-->
+
+    </body>
+
+</html>
---
+++ Modules/Base/Resources/views/layouts/partials/mt-header.blade.php
@@ -0,0 +1,57 @@
+<div id="kt_header" class="header bg-white header-fixed">
+
+    <!--begin::Container-->
+    <div class="container-fluid d-flex align-items-stretch justify-content-between">
+
+        <!--begin::Left-->
+        <div class="d-flex align-items-stretch mr-2">
+            <!--begin::Page Title-->
+            <h3 class="d-none text-dark d-lg-flex align-items-center mr-10 mb-0">@yield('page-title')</h3>
+            <!--end::Page Title-->
+        </div>
+        <!--end::Left-->
+
+        <!--begin::Topbar-->
+        <div class="topbar">
+
+            <!--begin::User-->
+            <div class="topbar-item">
+                <div class="dropdown dropdown-inline mr-2 show">
+                    <button type="button" class="btn btn-light-primary font-weight-bolder dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <?php
+                            $loggedHeaderUserName = 'Hi';
+                            if (session()->has('authUserData')) {
+                                $sessionUser = session('authUserData');
+                                $loggedHeaderUserName .= ', ' . ucwords($sessionUser['name']);
+                            }
+                        ?>
+                        <?= $loggedHeaderUserName; ?>
+                    </button>
+                    <!--begin::Dropdown Menu-->
+                    <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right show" x-placement="top-end" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(-62px, -165px, 0px);">
+                        <!--begin::Navigation-->
+                        <ul class="navi flex-column navi-hover py-2">
+
+                            <li class="navi-item">
+                                <a href="{{ route('userauth.logout') }}" class="navi-link">
+                                    <span class="navi-icon">
+                                        <i class="fas fa-sign-out-alt text-danger mr-5"></i>
+                                    </span>
+                                    <span class="navi-text">Logout</span>
+                                </a>
+                            </li>
+
+                        </ul>
+                        <!--end::Navigation-->
+                    </div>
+                    <!--end::Dropdown Menu-->
+                </div>
+            </div>
+            <!--end::User-->
+        </div>
+        <!--end::Topbar-->
+
+    </div>
+    <!--end::Container-->
+
+</div>
