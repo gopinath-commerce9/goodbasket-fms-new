63dcb4db37e081c48ee2fa42904ae0213267bba2
Gopinath R <gopicommerce9@gmail.com>
2021-11-01 12:05:22 +0530

    "Picker" module for "Picker" UserRole.

---
+++ Modules/Picker/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('picker::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('picker.name') !!}
+    </p>
+@endsection
